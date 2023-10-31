b3932b6bd3ca27f10f706832477cda62a1c499c2
Bobthecat2 <samuel.bd.lim@gmail.com>
2023-11-01 06:51:05 +0900

    logout implemented

--- components/screens/profile/ProfileScreen.tsx
+++ components/screens/profile/ProfileScreen.tsx
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { SafeAreaView, StyleSheet, ScrollView } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
 import { Colors } from "@/constants/Colors";
+import { useNavigation } from "@react-navigation/native";
+import { ScreenNavigationProp } from "@/typings/StackParam";
 import { TopImage } from "@/components/common/profile/index";
 import { Bio, Activities } from "@/components/layouts/profile/index";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { tokenAtom, loginInfoAtom } from "@/state/atoms/login";
+import { useRecoilValue, useSetRecoilState, useRecoilState } from "recoil";
+import { tokenAtom, loginInfoAtom, isLoggedInAtom } from "@/state/atoms/login";
 import { getProfileInfoApi } from "@/api/profile";
 import { profileAtom } from "@/state/atoms/profileEdit";
 import { isLoadingAtom } from "@/state/atoms/loading";
@@ -17,9 +27,12 @@ export interface IProfile {
 const ProfileScreen: React.FC = () => {
   const setIsLoading = useSetRecoilState(isLoadingAtom);
 
-  const loginInfoValue = useRecoilValue(loginInfoAtom);
-  const tokenValue = useRecoilValue(tokenAtom);
-  const setProfile = useSetRecoilState(profileAtom);
+  const navigation = useNavigation<ScreenNavigationProp>();
+
+  const [tokenValue, setTokenValue] = useRecoilState(tokenAtom);
+  const [loginInfoValue, setLoginInfoValue] = useRecoilState(loginInfoAtom);
+  const [profile, setProfile] = useRecoilState(profileAtom);
+  const [isLoggedIn, setIsLoggedIn] = useRecoilState(isLoggedInAtom);
 
   const handleProfileApi = async () => {
     setIsLoading(true);
@@ -30,16 +43,34 @@ const ProfileScreen: React.FC = () => {
     } = res;
     setProfile({ bio, profile_img, posts });
   };
+
+  const handleLogout = () => {
+    setTokenValue(null);
+    setLoginInfoValue({ username: "", nickname: "", type: "", phone_no: "" });
+    setProfile({ posts: [] });
+    setIsLoggedIn(false);
+    Alert.alert("Logged Out", "You are successfully logged out.", [
+      { text: "OK", onPress: () => navigation.navigate("Resource") },
+    ]);
+  };
+
   useEffect(() => {
     handleProfileApi();
   }, []);
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <TopImage nickname={loginInfoValue.nickname} />
-        <Bio />
-        <Activities />
-      </ScrollView>
+      <View style={styles.contentContainer}>
+        <ScrollView style={styles.scrollView}>
+          <TopImage nickname={loginInfoValue.nickname} />
+          <Bio />
+          <Activities />
+        </ScrollView>
+        <View style={styles.logoutContainer}>
+          <TouchableOpacity onPress={handleLogout}>
+            <Text style={styles.logoutText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -47,8 +78,25 @@ const ProfileScreen: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
     backgroundColor: Colors.white,
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 16,
+  },
+  scrollView: {
+    flexGrow: 1,
+  },
+  logoutContainer: {
+    padding: 16,
+  },
+  logoutText: {
+    fontSize: 13,
+    color: Colors.primary,
+    fontFamily: "PlusJakartaSans-Bold",
+    lineHeight: 18.2,
+    letterSpacing: 0.39,
+    textDecorationLine: "underline",
   },
 });
 
